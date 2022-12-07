6ff90720ef429e568c093a35d66482194fcefe9f
shahidCT <shahid.ansari@claimtherapist.com>
2022-12-07 23:51:19 +0530

    footer added to contact us page

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Contactus1 from '../components/contactus/Contactus1';
 import Contactus2 from '../components/contactus/Contactus2';
+import Footer from '../components/footer/Footer';
 
 const Contactus = () => {
   return (
     <div className='h-screen'>
       <Contactus1 />
       <Contactus2 />
+      <Footer />
     </div>
   );
 };
